c10cebeddf360aef7d0c6778d799c060cb39fba3
Boris Pocev <borispocevv@gmail.com>
2026-01-28 11:50:28 +0100

    Add Docker support and PostgreSQL configuration for Render deployment

--- FitnessTracker.Infrastructure/DependencyInjection.cs
+++ FitnessTracker.Infrastructure/DependencyInjection.cs
@@ -18,7 +18,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
     {
         services.AddDbContext<AppDbContext>(opt =>
-            opt.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"))); 
 
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IExerciseService, ExerciseService>();
