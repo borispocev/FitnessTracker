ab7172526b23f036da20a005d4c2b797571af78c
Boris Pocev <borispocevv@gmail.com>
2026-02-08 16:43:28 +0100

    Docker + CI + k8s manifest

---
+++ src/FitnessTracker.Api/Program.cs
@@ -0,0 +1,29 @@
+﻿using FitnessTracker.Infrastructure;
+using FitnessTracker.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
